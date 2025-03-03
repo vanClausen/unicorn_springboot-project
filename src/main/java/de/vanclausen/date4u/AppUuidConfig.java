298eb7076ecf60f0db5966d784f0403d0300c029
Sebastian Claus <claus.aka.crock@googlemail.com>
2025-03-03 23:41:22 +0100

    change appUuid bean to return string. add shorterAppUuid bean

--- src/main/java/de/vanclausen/date4u/AppUuidConfig.java
+++ src/main/java/de/vanclausen/date4u/AppUuidConfig.java
@@ -13,9 +13,16 @@ public class AppUuidConfig {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Bean
-    public UUID uuid() {
-        UUID uuid = UUID.randomUUID();
+    public String appUuid() {
+        String uuid = UUID.randomUUID().toString();
         log.info("Generated UUID -> {}", uuid);
         return uuid;
+    }
+
+    @Bean
+    public String shorterAppUuid() {
+        String shorterUuid = appUuid().substring(0, appUuid().length() / 2);
+        log.info("Shortened UUID -> {}", shorterUuid);
+        return shorterUuid;
     }
 }
