d9d9616432d77f2ced15ffe7c9acf4850481cc55
Sebastian Claus <claus.aka.crock@googlemail.com>
2025-03-03 23:52:41 +0100

    add Logger to PromptProviderConfig

--- src/main/java/de/vanclausen/date4u/shell/PromptProviderConfig.java
+++ src/main/java/de/vanclausen/date4u/shell/PromptProviderConfig.java
@@ -2,11 +2,15 @@ package de.vanclausen.date4u.shell;
 
 import org.jline.utils.AttributedString;
 import org.jline.utils.AttributedStyle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.shell.jline.PromptProvider;
 
 @Configuration
 public class PromptProviderConfig implements PromptProvider {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     private static final PromptProvider userPromptProvider = () -> new AttributedString(
             "date4u:>", AttributedStyle.DEFAULT.foreground(AttributedStyle.GREEN)
@@ -18,6 +22,7 @@ public class PromptProviderConfig implements PromptProvider {
     @Override
     public AttributedString getPrompt() {
         boolean isAdmin = (Math.round(Math.random()) == 1);
+        log.info("isAdmin: {}", isAdmin);
         return isAdmin ? adminPromptProvider.getPrompt() : userPromptProvider.getPrompt();
     }
 }
